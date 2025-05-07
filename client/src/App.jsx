703fa46450be5bff87d6d09ef0c904630dd9c82d
sanket0702 <129685508+sanket0702@users.noreply.github.com>
2025-05-08 03:16:13 +0530

    root

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
 
         {/* 🔐 Protected Routes */}
