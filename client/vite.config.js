19c1848bbf4df4185e447d784acd87cdaf59619c
sanket0702 <129685508+sanket0702@users.noreply.github.com>
2025-05-12 16:57:32 +0530

    added and upgrade css to support every device

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Make sure this matches your file structure
-  ],
-  theme: {
-    extend: {},
-  },
   plugins: [react(), tailwindcss(),],
 })
