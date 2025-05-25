9f5ade7d190cb53fe22e04e343ba6647567b4db5
sanket0702 <129685508+sanket0702@users.noreply.github.com>
2025-05-25 18:04:08 +0530

    added update

--- client/src/pages/StatementPage/StatementPage.jsx
+++ client/src/pages/StatementPage/StatementPage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
+
 import Logo from '../../assets/logo.svg'
 const TransactionStatement = () => {
   const [transactions, setTransactions] = useState([]);
@@ -39,18 +38,6 @@ const TransactionStatement = () => {
 }, []);
 
 
-  const downloadPDF = () => {
-    const input = statementRef.current;
-    html2canvas(input, { scale: 2 }).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('p', 'mm', 'a4');
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
-      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-      pdf.save('Bank_Transaction_Statement.pdf');
-    });
-  };
-
   return (
     <div className="p-4 md:p-6 font-sans bg-gray-100 min-h-screen">
   <div ref={statementRef} className="bg-white p-4 md:p-6 shadow-lg rounded-md">
@@ -58,9 +45,9 @@ const TransactionStatement = () => {
     {/* Header Section */}
     <div className="mb-4 md:mb-6 border-b pb-4">
       <h1 className="flex items-center h-[40px] w-[110px] bg-red-600">
-  <img src={Logo} alt="Bank of NextGen Logo" className="h-[35px] w-auto object-cover " />
+  <img src={Logo} alt="Bank of NextGen Logo" className="h-[30px] w-[100px] object-cover" />
 </h1>
-      <div className="text-xs md:text-sm text-gray-600 space-y-1">
+      <div className="text-xs md:text-sm text-gray-600  space-y-1">
         <p><strong>Name:</strong> {userInfo.fullName || 'John Doe'}</p>
         <p><strong>Account Number:</strong> {userInfo.accountNumber || 'XXXX-XXXX-XXXX-1234'}</p>
         <p><strong>Email:</strong> {userInfo.email || 'john@example.com'}</p>
@@ -121,12 +108,7 @@ const TransactionStatement = () => {
     </p>
   </div>
 
-  <button
-    onClick={downloadPDF}
-    className="mt-4 md:mt-6 px-4 md:px-6 py-2 bg-blue-700 text-white rounded-md hover:bg-blue-800 w-full md:w-auto"
-  >
-    Download PDF Statement
-  </button>
+  
 </div>
 
 
